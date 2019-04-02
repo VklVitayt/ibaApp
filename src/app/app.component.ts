72b146c09c8856ae533cc61c6509e5835db1da0e
Igor <43810546+VklVitayt@users.noreply.github.com>
2019-04-02 22:13:50 +0300

    course project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ibaApp';
+  title = 'frontend';
+
+  constructor(private  http: HttpClient) {
+
+  }
+
+  public clickFunction(): void {
+    this.http.get('/api/').subscribe(value => {
+      console.log(value);
+    });
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {PagesModule} from './modules/pages/pages.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
